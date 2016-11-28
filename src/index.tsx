5cd7937e416b333ba804d52f958bda0bbd9086b1
Frank Schmitz <frank.schmitz@rewe-digital.com>
2016-11-28 16:27:45 +0100

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {Home} from "./components/Home";
+
+ReactDOM.render(<Home name="Frank" age={24}/>, document.getElementById("app"));
